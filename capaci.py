94a8942a8d0ec308d44d221b219a01aef3f5aa15
ArchLemos <51244569+CabeludoCego@users.noreply.github.com>
2022-02-28 12:49:03 -0300

    Pergunta do horário

--- capaci.py
+++ capaci.py
@@ -1,3 +1,8 @@
 print("Ola, mundo")
 print("Olá, José Henrique")
 print(1+2)
+
+hrs = int(input("Digite que horas são:"))
+print("São ",hrs, " horas.")
+min = int(input("Digite quantos minutos são:"))
+print("São {} minutos".format(min))
