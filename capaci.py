2a6e648c88e399a73dbbf7a98663b12db754d7da
Mariana Moura <77185247+marianasmoura@users.noreply.github.com>
2022-03-02 02:16:29 -0300

    Atualização

--- capaci.py
+++ capaci.py
@@ -1,3 +1,6 @@
+from xml.dom.minidom import CharacterData
+
+
 print("Ola, mundo")
 print("Olá, José Henrique")
 print(1+2)
@@ -6,3 +9,12 @@ hrs = int(input("Digite que horas são:"))
 print("São ",hrs, " horas.")
 min = int(input("Digite quantos minutos são:"))
 print("São {} minutos".format(min))
+
+num = int(input("Digite um número: "))
+if num == 0:
+    print("Você digitou 0!")
+elif num < 0:
+    print("Você digitou um número menor que zero!")
+else:
+    print("Você digitou um número maior que zero!")
+
